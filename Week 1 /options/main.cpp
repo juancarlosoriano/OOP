bc6f06a537ebe58b166cac63ca20e34a4753e491
JC Soriano <juancarlosoriano18@gmail.com>
2024-01-07 02:29:16 -0500

    First commit

---
+++ Week 1 /options/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* Build options list */
+int main() {
+  // 1 - Print Help
+
+  // 2 - Print exchange stats
+
+  // 3 - Make an offer
+
+  // 4 - Make a bid
+
+  return 0;
+}
